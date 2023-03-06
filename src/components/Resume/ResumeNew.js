0c0ebd4a9bd46c3f680eee4c252d2afc51ce0451
Felicity Brammer <felicitybrammer@gmail.com>
2023-03-05 18:10:15 -0800

    updated portfolio

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/FB_2022_RESUME.pdf";
+import pdf from "../../Assets/FB_2023_RESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "https://raw.githubusercontent.com/felicitybrammer/wild-port/main/src/Assets/FB_2022_RESUME.pdf";
+const resumeLink = "https://raw.githubusercontent.com/felicitybrammer/wild-port/main/src/Assets/FB_2023_RESUME.pdf";
 
 
 function ResumeNew() {
